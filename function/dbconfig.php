93997ce375db4ed26206e294f4418b5e63ba5b42
Baransel Pamuk <38981711+baranselpamuk@users.noreply.github.com>
2023-10-12 13:11:59 +0300

    Update dbconfig.php

--- function/dbconfig.php
+++ function/dbconfig.php
@@ -1,9 +1,9 @@
 <?php
 // Veritabanı bağlantısı için gerekli bilgiler
 $host = "localhost"; // Veritabanı sunucusunun adresi
-$username = "tilkisof_event"; // Veritabanı kullanıcı adı
-$password = "baransel2023++"; // Veritabanı şifresi
-$database = "tilkisof_event"; // Kullanılacak veritabanının adı
+$username = "database_name"; // Veritabanı kullanıcı adı
+$password = "database_pass"; // Veritabanı şifresi
+$database = "database"; // Kullanılacak veritabanının adı
 
 // Veritabanı bağlantısını oluştur
 $mysqli = new mysqli($host, $username, $password, $database);
